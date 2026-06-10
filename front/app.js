6089781a58007717995c781b1c47f141ea0be2ea
timebe00 <an0160@naver.com>
2026-06-11 00:23:38 +0900

    세팅

--- front/app.js
+++ front/app.js
@@ -4,38 +4,58 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+setApp = () => {
+  return new Promise(
+    async (resolve, reject) => {
+      try {
+        global._base = __dirname;
+        global._ServiceType = process.env.ServiceType || 'production';
+        global._props = require(__dirname + '/config/' + _ServiceType + '/property.js');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+        global._mybatisMapper = require('mybatis-mapper');
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+        var indexRouter = require('./routes/index');
+        var usersRouter = require('./routes/users');
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+        // view engine setup
+        app.set('views', path.join(__dirname, 'views'));
+        app.set('view engine', 'ejs');
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+        app.use(logger('dev'));
+        app.use(express.json());
+        app.use(express.urlencoded({ extended: false }));
+        app.use(cookieParser());
+        app.use(express.static(path.join(__dirname, 'public')));
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+        app.use('/', indexRouter);
+        app.use('/users', usersRouter);
 
-module.exports = app;
+        // catch 404 and forward to error handler
+        app.use(function(req, res, next) {
+          next(createError(404));
+        });
+
+        // error handler
+        app.use(function(err, req, res, next) {
+          // set locals, only providing error in development
+          res.locals.message = err.message;
+          res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+          // render the error page
+          res.status(err.status || 500);
+          res.render('error');
+        });
+
+        resolve(app);
+      }
+      catch (error) {
+        console.log("error : ", error)
+        reject(error);
+      }
+    }
+  )
+};
+
+module.exports = setApp();
